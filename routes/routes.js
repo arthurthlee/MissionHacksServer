43ff4fc666f7875514450c01e82de15c92eb3b87
ruhtraeel <ruhtraeel@gmail.com>
2018-03-18 11:06:56 -0700

    Undid null checks

--- routes/routes.js
+++ routes/routes.js
@@ -63,12 +63,12 @@ var appRouter = function (app, droneFleet, warehouseLocations, stationLocations)
       }
 
       //console.log("Drone Location : " + JSON.stringify(droneLocation));
-      if (droneLocation && droneLocation.lat == orderInfo.lat
+      if (droneLocation.lat == orderInfo.lat
         && droneLocation.long == orderInfo.long
         && droneLocation.alt == orderInfo.alt) {
       res.status(200).json({ status: 'arrived' });
         }
-        else if (droneLocation) {
+        else {
       res.status(200).json(Object.assign( droneLocation, { status: 'transit' } ) );
         }
     });
