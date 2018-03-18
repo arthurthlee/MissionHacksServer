d5358c6352a2d44751c14c6fab53e9e5940cb954
ruhtraeel <ruhtraeel@gmail.com>
2018-03-17 21:29:44 -0700

    Finished drone straight line algorithm

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,5 @@
 var userOrders = [];
-var locations = [];
+var droneLocations = [];
 var currentOrderId = 0;
 
 var appRouter = function (app) {
@@ -8,66 +8,71 @@ var appRouter = function (app) {
     });
 
     app.get("/packageLocation/:orderId", function (req, res) {  
-      console.log("Locations : " + JSON.stringify(locations, null, 2));
+      console.log("Drone Locations : " + JSON.stringify(droneLocations, null, 2));
       console.log("Request orderId: " + req.params.orderId);
-      var locationForOrder = locations.filter(function(location) {
+      var locationForOrder = droneLocations.filter(function(location) {
         
         return Number( location.userOrderId ) === Number( req.params.orderId );
 
       })[ 0 ];
+      var orderInfo = userOrders.filter(function(order) {
+        return Number(order.userOrderId) === Number(req.params.orderId);
+      }) [0];
 
-      console.log("Location for Order : " + locationForOrder);
-      if (locationForOrder.lat == req.params.lat
-        && locationForOrder.long == req.params.long
-        && locationForOrder.alt == req.params.long) {
+      console.log("Location for Order : " + JSON.stringify(locationForOrder));
+      if (locationForOrder.lat == orderInfo.lat
+        && locationForOrder.long == orderInfo.long
+        && locationForOrder.alt == orderInfo.long) {
       res.status(200).json("PACKAGE ARRIVED");
         }
         else {
       res.status(200).json(locationForOrder);
         }
-      if (locationForOrder.lat < req.params.lat) {
+      if (orderInfo.lat > locationForOrder.lat) {
         locationForOrder.lat++;
       }
-      else {
+      else if (orderInfo.lat < locationForOrder.lat) {
         locationForOrder.lat--;
       }
-      if (locationForOrder.long < req.params.lat) {
+      if (orderInfo.long > locationForOrder.long) {
         locationForOrder.long++;
       }
-      else {
+      else if (orderInfo.long < locationForOrder.long) {
         locationForOrder.long--;
       }
-      if (locationForOrder.alt <req.params.lat) {
+      if (orderInfo.alt > locationForOrder.alt) {
         locationForOrder.alt++;
       }
-      else {
+      else if (orderInfo.alt < locationForOrder.alt) {
         locationForOrder.alt--;
       }
     });
 
     app.post("/placeOrder", function (req, res) {
+      var orderId = currentOrderId;
       var userOrder = ({
           lat: req.body.lat,
           long: req.body.long,
           alt: req.body.alt,
           userId: req.body.userId,
           productId: req.body.productId,
-          timeOfOrder: req.body.timeOfOrder
+          timeOfOrder: req.body.timeOfOrder,
+          userOrderId: orderId 
       });
-      var orderId = currentOrderId;
+
        userOrders.push(userOrder);
        res.status(201).json(orderId);
       currentOrderId++;
-       // Mock location data
+       // Mock warehouse location data
        var data = {
         userOrderId: orderId, 
         lat: Math.round(Math.random()*100),
         long: Math.round(Math.random()*100),
         alt: Math.round(Math.random()*20)
       };
-      locations.push(data);
-      console.log(locations);
-      console.log(userOrders);
+      droneLocations.push(data);
+      console.log("Drone Locations" + JSON.stringify(droneLocations, null, 2));
+      console.log("UserOrderLocations " + JSON.stringify(userOrders, null, 2));
        // Optimization logic to send out drone
   });
   }
